acc07aa24a4d1d7131b53e7b53d1b12143b62d1a
will chen <willchen0011@gmail.com>
2025-10-29 14:08:48 +0800

    remove version filter buttons from cluster page

--- web/src/components/ClusterStatus.vue
+++ web/src/components/ClusterStatus.vue
@@ -36,30 +36,6 @@
       </div>
     </div>
 
-    <!-- Quick Actions -->
-    <div class="mb-4 flex flex-wrap items-center gap-2">
-      <button
-        @click="filterVersionMismatch"
-        class="px-3 py-1.5 text-sm font-medium text-orange-700 bg-orange-100 border border-orange-300 rounded-lg hover:bg-orange-200 transition-colors"
-        :class="{ 'bg-orange-200 border-orange-400': versionFilter === 'mismatch' }"
-      >
-        <svg class="w-4 h-4 inline mr-1" fill="currentColor" viewBox="0 0 20 20">
-          <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-        </svg>
-        Version Mismatch ({{ getVersionMismatchCount() }})
-      </button>
-      <button
-        @click="filterUnknownVersion"
-        class="px-3 py-1.5 text-sm font-medium text-gray-700 bg-gray-100 border border-gray-300 rounded-lg hover:bg-gray-200 transition-colors"
-        :class="{ 'bg-gray-200 border-gray-400': versionFilter === 'unknown' }"
-      >
-        <svg class="w-4 h-4 inline mr-1" fill="currentColor" viewBox="0 0 20 20">
-          <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
-        </svg>
-        Unknown Version ({{ getUnknownVersionCount() }})
-      </button>
-    </div>
-
     <!-- Filter Summary -->
     <div v-if="versionFilter !== 'all' || searchQuery" class="mb-4 p-3 bg-blue-50 border border-blue-200 rounded-lg">
       <div class="flex items-center justify-between">
